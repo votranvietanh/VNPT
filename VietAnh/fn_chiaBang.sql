00197dc46df7f00623a70e7b0241048e9f0b11f3
Viet Anh <93200025+votranvietanh@users.noreply.github.com>
2024-09-13 14:39:16 +0700

    Update fn_chiaBang.sql

--- VietAnh/fn_chiaBang.sql
+++ VietAnh/fn_chiaBang.sql
@@ -1,4 +1,30 @@
-pi
+                        BEGIN
+                            create_kpi_summary('MDHT', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'MUCDO_HOANTHANH');
+                            create_kpi_summary('giao', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'giao');
+                            create_kpi_summary('THUCHIEN', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'THUCHIEN');
+                            create_kpi_summary('TLTH', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'TYLE_THUCHIEN');
+                            create_kpi_summary('DIEM_CONG', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'DIEM_CONG');
+                            create_kpi_summary('DIEM_TRU', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'DIEM_TRU');
+                        END;
+                        /
+
+--===================================================================================================================================================
+
+
+CREATE OR REPLACE PROCEDURE create_kpi_summary (
+    p_mdhd     IN VARCHAR2,  -- tên phân biệt (MDHD, giao, thuchien, cong, tru)
+    p_thang    IN NUMBER,    -- Tháng 
+    p_mucdo    IN VARCHAR2   -- tên cột trong ttkd_bsc.bangluong where thang = ?
+) AUTHID CURRENT_USER AS
+    v_sql VARCHAR2(32767);
+BEGIN
+    -- Tạo bảng mới với các cột KPI
+    v_sql := 'CREATE TABLE kpi_summary__' || p_mdhd || '' || p_thang || ' AS ' || 
+             'SELECT ma_nv, ten_nv, ma_vtcv, ten_vtcv, ma_to, ten_to, ma_pb, ten_pb, ';
+
+    -- Thêm các cột KPI vào câu lệnh SQL
+    FOR rec IN (
+        SELECT DISTINCT ma_kpi
         FROM ttkd_bsc.bangluong_kpi
         WHERE thang = p_thang  -- sử dụng biến tháng
     ) LOOP
@@ -18,12 +44,3 @@ pi
 END;
 /
 
-BEGIN
-    create_kpi_summary('MDHT', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'MUCDO_HOANTHANH');
-    create_kpi_summary('giao', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'giao');
-    create_kpi_summary('THUCHIEN', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'THUCHIEN');
-    create_kpi_summary('TLTH', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'TYLE_THUCHIEN');
-    create_kpi_summary('DIEM_CONG', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'DIEM_CONG');
-    create_kpi_summary('DIEM_TRU', TO_NUMBER(TO_CHAR(SYSDATE, 'YYYYMM')), 'DIEM_TRU');
-END;
-/
