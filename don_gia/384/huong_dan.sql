c4973297e9fc23cb460b3ddd5c6b76e8e94514cf
Viet Anh <93200025+votranvietanh@users.noreply.github.com>
2024-10-31 19:35:33 +0700

    Update huong_dan.sql

--- don_gia/384/huong_dan.sql
+++ don_gia/384/huong_dan.sql
@@ -1,7 +1,21 @@
 BHOL: ap dung từ tháng 9
 1- Cto P: loai ds card to post ra vi a Tuyền đã tính PTM
-2- Post --> Card: Khanh có thông  tin lý do huy chuyển tỉnh
+        select SOMAY 
+        From TTKD_BSC.DT_PTM_VNP_202409
+        where KIEU_LD = 'Chuyen C->P'
+        ;
 3- Post PTM thang n-1 khong gói đã chi 60k, thì tháng n mua gói không chi --> a Tuyền
+        select SOMAY 
+        From TTKD_BSC.DT_PTM_VNP_202409
+        where LOAI_GOI = 'GOI KHONG KM'
 4- Loại các tbao thang n-1 là U1500/U900 --> anh Tuyền
+      --916
+      
 5- Loại thue bao lương tính (a Tuyền)
-6- không khó khăn không thể nhận diện và xử lý dc
+       select SOMAY 
+        From TTKD_BSC.DT_PTM_VNP_202409
+        WHERE GOI_LUONGTINH IS NOT NULL
+      ;
+2- Post --> Card: Khanh có thông  tin lý do huy chuyển tỉnh
+        Select thang, kieu_ld, somay From khanhtdt_ttkd.CCBS_ID1896@TTKDDB Where thang = 202409 and kieu_ld = 'P -> C';
+        Select thang, kieu_ld, somay From khanhtdt_ttkd.CCBS_ID1896_CT@TTKDDB Where thang = 202409;
