22a663c6e4b9479f46611700eb76ea9383507eec
Viet Anh <93200025+votranvietanh@users.noreply.github.com>
2024-11-25 13:33:47 +0700

    Delete VietAnh/sumary.sql

--- VietAnh/sumary.sql
+++
@@ -1,2 +0,0 @@
-select ma_kpi,nguoi_xuly, nvl(GIAO,0)+nvl(THUCHIEN,0)+nvl(TYLE_TH,0)+nvl(MUCDO_HT,0)+nvl(DIEMCONG,0)+nvl(DIEMTRU,0) checkk from  ttkd_bsc.blkpi_danhmuc_kpi where thang = 202408 
-order by nvl(GIAO,0)+nvl(THUCHIEN,0)+nvl(TYLE_TH,0)+nvl(MUCDO_HT,0)+nvl(DIEMCONG,0)+nvl(DIEMTRU,0);
